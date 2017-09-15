3ae96571dcf9c69835d6ab86b4dfe769458e1042
voidblue <kimqwe12300@daum.net>
2017-09-15 18:07:04 +0900

    2017-09-15

---
+++ 02/app.js
@@ -0,0 +1,43 @@
+// 테스트
+// var str = "hello world";
+// console.log(str);
+// var count = 5;
+// console.log(count);
+
+
+
+// 조건문
+// var isMan = true;
+//
+// if(isMan){
+//   console.log('남자다');
+// }
+// else{
+//   console.log('\'남자\' 아님;');
+// }
+
+// 함수만들기
+// function printHello(name){
+//   console.log('hello, ' + name + '!')
+// }
+// printHello('Jaeyun')
+
+var person = {
+  'age' : 24,
+  'name' : 'KimJaeYun',
+  'area' : "Jeju",
+  cry: function(){
+    console.log("엉엉");
+  }
+}
+person.cry();
+
+image = {};
+image['width'] = 1;
+image['name'+1] = '1.jpg';
+
+image = {width:1, height:2};
+
+list = [];
+list[0] = '1.jpg';
+list[1] = '2.jpg';
---
+++ 02/prompt_sum.js
@@ -0,0 +1,22 @@
+notnull = true;
+list = [];
+pointer = 0;
+while(notnull){
+  var num = window.prompt('숫자를 받습니다. Cancel버튼을 누르면 모두 더합니다.')
+  if(xxx != null){
+    list[pointer] = parseInt(xxx);
+    pointer++;
+  }
+  else{
+    notnull = false;
+  }
+}
+getprompt(list, pointer);
+
+function getprompt(list, pointer){
+  var sum = 0;
+  for (var x = 0 ; x < pointer ; x++){
+    sum += list[x];
+  }
+  alert("더한 값은 "+sum+"입니다.");
+}
